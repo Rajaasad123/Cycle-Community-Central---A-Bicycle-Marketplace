a6546318b02c1fa325037020d1a03195d26567af
MohammedWasifAhmedKhan <arbaaz18@gmail.com>
2023-11-29 17:17:05 +0000

    Updating project repo with db.js file

---
+++ db.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var mysql = require('mysql')
+var router = express.Router()
+
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database:'bycyclecommunity'
+})
+
+db.connect(function(err){
+    if(err) throw err
+
+    console.log('Database connected!')
+})
+
+module.exports = db
