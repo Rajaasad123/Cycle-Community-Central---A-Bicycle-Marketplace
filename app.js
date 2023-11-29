22e1b5f103ee472d1fe1d9fad0159ff51d7a7eb8
Qaiser Ali (Student) <aliq1@roehampton.ac.uk>
2023-11-29 15:43:14 +0000

    Updating project repo with app.js file (written in Express.js)

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express();
+var db = require('./Database/db')
+var path = require('path')
+var router = require('./Router/index')
+
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','pug')
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', router)
+app.get('/bicycleDetail', router)
+
+
+app.listen('5000',function(err){
+    if(err) throw err
+
+    console.log('Server started at port 5000')
+    console.log('You can access the webpage using http://localhost:5000')
+})
