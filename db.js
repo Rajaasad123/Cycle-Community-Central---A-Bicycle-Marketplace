469b0f5eac5a7f0917fda0bc00f151408952220e
Rajaasad123 <147387485+Rajaasad123@users.noreply.github.com>
2024-01-05 10:31:30 +0000

    Pushing latest file of db.js.

--- db.js
+++ db.js
@@ -2,15 +2,23 @@ var express = require('express')
 var mysql = require('mysql')
 var router = express.Router()
 
+
+var mysqlHost = process.env.MYSQL_HOST || 'localhost';
+var mysqlPort = process.env.MYSQL_PORT || '3306';
+var mysqlUser = process.env.MYSQL_USER || 'root';
+var mysqlPass = process.env.MYSQL_PASS || '';
+var mysqlDB   = process.env.MYSQL_DB   || 'bycyclecommunity';
+
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database:'bycyclecommunity'
+    host: mysqlHost,
+    user: mysqlUser,
+    password: mysqlPass,
+    port: mysqlPort,
+    database: mysqlDB
 })
 
 db.connect(function(err){
+    // console.log("Database",db);
     if(err) throw err
 
     console.log('Database connected!')
