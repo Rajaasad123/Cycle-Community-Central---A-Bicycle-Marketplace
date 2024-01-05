06bb4ce69e841f115f810288703d40bddedfdfd5
Alih24 <146723831+Alih24@users.noreply.github.com>
2024-01-05 00:09:02 +0000

    Updating project repo with file of app.js

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ var router = require('./Router/index')
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','pug')
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', router)
 app.get('/index', router)
 app.get('/bicycleDetail', router)
-
+app.get('/register', router)
+app.post('/register', router)
+app.get('/login', router)
+app.post('/login', router)
+app.get('/profile', router)
+app.post('/profile', router)
+app.get('/addnewbicycle', router)
+app.post('/addnewbicycle', router)
+app.get('/logout', router)
+app.get('/deletebycycle', router)
 
 app.listen('5000',function(err){
     if(err) throw err
